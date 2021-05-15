4399ad9ff13aa9e3e34d9f29d306e3336ffebd56
atique <atiqurrahman7465@gmail.com>
2021-05-15 10:48:58 +0600

    Initial commit

---
+++ src/test/java/com/atique/springbootsecuritypractice/SpringBootSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atique.springbootsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
