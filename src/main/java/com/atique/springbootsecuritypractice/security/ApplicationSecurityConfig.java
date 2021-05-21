a171b9b7541d4e6f8187fd69e3213db911853956
atique <atiqurrahman7465@gmail.com>
2021-05-21 17:30:02 +0600

    [Spring-Security] - using CSRF.

--- src/main/java/com/atique/springbootsecuritypractice/security/ApplicationSecurityConfig.java
+++ src/main/java/com/atique/springbootsecuritypractice/security/ApplicationSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import static com.atique.springbootsecuritypractice.enums.ApplicationUserRole.*;
 
@@ -30,7 +31,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
