96e70fd284f36a56df33a006e4bbb43a2b89b880
atique <atiqurrahman7465@gmail.com>
2021-05-26 22:52:28 +0600

    [Spring-Security] - JWT authentication.

---
+++ src/main/java/com/atique/springbootsecuritypractice/jwt/jwtSecretKey.java
@@ -0,0 +1,23 @@
+package com.atique.springbootsecuritypractice.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class jwtSecretKey {
+    private final JwtConfig jwtConfig;
+
+    @Autowired
+    public jwtSecretKey(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes());
+    }
+}
